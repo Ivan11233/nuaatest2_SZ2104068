79d39467d9ff468e1372a802416c9743db1a2608
ivan1008 <40709571+ivan1008@users.noreply.github.com>
2022-08-12 00:08:30 +0800

    Update SimpleCalculation.java
    这是开发者第一次给审查者的提交

--- src/java/SimpleCalculation.java
+++ src/java/SimpleCalculation.java
@@ -1,4 +1,7 @@
 public class SimpleCalculation {
+    public static void main(String[] args）{
+    System.Out.Println("这是要被审查者拒绝的改动")；
+    }
     public int Add(int a,int b){
         return (a+b);
     }
@@ -7,3 +10,4 @@ public class SimpleCalculation {
         return(a-b);
     }
 }
+#SZ2104068，代码审查作业
